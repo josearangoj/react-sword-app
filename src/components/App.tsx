466f8835eecc48d12097ff35bc0599b34bfc1c33
Jose Arango <josearango1202@gmail.com>
2022-07-14 12:39:59 -0400

    Lab6 on class work

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,37 +1,30 @@
-import "./App.css";
-import React, { useState } from "react";
-import { Login } from "./Login/Login";
-import { CharacterList } from "./CharacterList/CharacterList";
+import { useState } from "react";
 import { useFetch } from "../hooks/useFetch";
-import { CharacterSelection } from "./CharacterSelection/CharacterSelection";
-
-//React application can be represented as a tree of React components
-//This is a react root component
-//This type of components is called functional components
-//Functional component should start with a capital letter,
-//return JSX and be exported from a file
-//try to abstain from default export
-
-//Let's create a functionality that only when user logged in as admin,
-//we can see the character list, otherwise we see the simple message like
-//"You are not logged in"
+import "./App.css";
+import { CharactersScreen } from "./screens/CharactersScreen";
+import { Login } from "./Login/Login";
+import { Battleground } from "./Battleground/Battleground";
+import { Text } from "@chakra-ui/react";
 
 export const App = () => {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [isFightGoingOn, setFightStart] = useState(false);
+  const [battleCharacters, setBattleCharacters] = useState([]);
+  const [winner, setWinner] = useState(null);
   const characters = [
     {
       name: "Goku",
       health: 100,
       fraction: "Saiyan",
       weapon: "Ki",
-      damagePerHit: 10,
+      damagePerHit: 25,
     },
     {
       name: "Bobrik",
       health: 150,
       fraction: "Random",
       weapon: "Bow",
-      damagePerHit: 6,
+      damagePerHit: 19,
     },
     {
       name: "Valera",
@@ -54,17 +47,30 @@ export const App = () => {
     //In react we can't render objects or arrays
     return <>Error: {error.message} </>;
   }
+  console.log("Selected characters", battleCharacters);
 
-  const userNotLoggedIn = (
-    <h3 className="not-logged-in">
-      Please log in as admin to see character list
-    </h3>
-  );
   return (
     <div className="App">
       {!isLoggedIn ? <Login setLoggedIn={setIsLoggedIn} /> : null}
-      {isLoggedIn ? <CharacterList characters={characters} /> : userNotLoggedIn}
-      {isLoggedIn ? <CharacterSelection characters={characters} /> : null}
+      {isLoggedIn && !isFightGoingOn ? (
+        <CharactersScreen
+          characters={characters}
+          setFightStart={setFightStart}
+          setBattleCharacters={setBattleCharacters}
+        />
+      ) : null}
+      {isFightGoingOn && !winner ? (
+        <Battleground
+          winner={winner}
+          setWinner={setWinner}
+          battleCharacters={battleCharacters}
+        />
+      ) : null}
+      {isFightGoingOn && winner ? (
+        <Text fontSize={"5xl"} fontWeight="800">
+          Winner of the battle is {winner}
+        </Text>
+      ) : null}
     </div>
   );
 };
